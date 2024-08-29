fd213cc15d75a50f8b8f959a8c38d73e3b84c8e9
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-30 00:58:26 +0600

    load menu and reviews data from server

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -14,7 +14,7 @@ import { FaQuoteLeft } from "react-icons/fa";
 const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
     useEffect(() => {
-        fetch('reviews.json')
+        fetch('http://localhost:8000/reviews')
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [])
