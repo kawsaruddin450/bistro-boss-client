0c99747a9889d20ecc1685e379b05d4b117d5061
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-29 11:24:33 +0600

    homepage banner implement and navbar design update

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <h1>Home Page coming soon...</h1>
+            <Banner></Banner>
         </div>
     );
 };
