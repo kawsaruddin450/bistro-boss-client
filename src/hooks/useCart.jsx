62570b9bf31fef12d5930fef4e46ff57d1b03637
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-30 20:20:37 +0600

    implement tanstack query and setup custom hook

---
+++ src/hooks/useCart.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+
+
+const useCart = () => {
+    const {user} = useContext(AuthContext);
+
+    const {isLoading, refetch,  data: cart = []} = useQuery({
+        queryKey: ['carts', user?.email],
+        queryFn: async ()=>{
+            const response = await fetch(`http://localhost:8000/carts?email=${user.email}`);
+            return response.json();
+        },
+    })
+    
+    return [cart, refetch]
+};
+
+export default useCart;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,22 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes.jsx'
 import { HelmetProvider } from 'react-helmet-async'
 import AuthProvider from './providers/AuthProvider.jsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { getTodos, postTodo } from '../my-api'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <AuthProvider><RouterProvider router={router} /></AuthProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </AuthProvider>
   </StrictMode>,
 )
