90bba49f8c5687976cbdd219eb6029dbe36ea84c
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-30 12:33:34 +0600

    firebase integration and setup auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes.jsx'
 import { HelmetProvider } from 'react-helmet-async'
+import AuthProvider from './providers/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <AuthProvider><RouterProvider router={router} /></AuthProvider>
     </HelmetProvider>
   </StrictMode>,
 )
