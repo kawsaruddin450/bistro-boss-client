68354134f892b271a32dbe9ed06db7ace98b2561
kawsar-uddin <kawsaruddin450@gmail.com>
2024-09-04 00:53:58 +0600

    delete menu item from server

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -3,10 +3,44 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle'
 import useMenu from "../../../hooks/useMenu";
 import { RiDeleteBinLine } from "react-icons/ri";
 import { FiEdit } from "react-icons/fi";
+import Swal from "sweetalert2";
+import { useEffect } from "react";
 
 
 const ManageItems = () => {
     const [menu, refetch] = useMenu();
+    const token = localStorage.getItem('access-token');
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:8000/menu/${id}`, {
+                    method: "DELETE",
+                    headers: {
+                        authorization: `bearer ${token}`
+                    }
+                }).then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount === 1) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "This item has been deleted.",
+                                icon: "success"
+                            });
+                            refetch();
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div className='w-full max-w-screen-md mx-auto my-12'>
             <Helmet>
@@ -22,8 +56,8 @@ const ManageItems = () => {
                             <th>Image</th>
                             <th>Item Name</th>
                             <th>Price</th>
-                            <th>Action</th>
-                            <th>Action</th>
+                            <th>Update</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -48,10 +82,10 @@ const ManageItems = () => {
                                 </td>
                                 <td className='text-end'>${item.price}</td>
                                 <td>
-                                    <button  className="btn btn-square btn-sm rounded-md text-md bg-[#D1A054] text-white"><FiEdit></FiEdit></button>
+                                    <button className="btn btn-square btn-sm rounded-md text-md bg-[#D1A054] text-white"><FiEdit></FiEdit></button>
                                 </td>
                                 <td>
-                                    <button  className="btn btn-square btn-sm rounded-md text-md bg-[#B91C1C] text-white"><RiDeleteBinLine></RiDeleteBinLine></button>
+                                    <button onClick={() => handleDelete(item._id)} className="btn btn-square btn-sm rounded-md text-md bg-[#B91C1C] text-white"><RiDeleteBinLine></RiDeleteBinLine></button>
                                 </td>
                             </tr>)
                         }
