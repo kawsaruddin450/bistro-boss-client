92b36f774d1ad1e91acb7063a7ce0cda15512824
kawsar-uddin <kawsaruddin450@gmail.com>
2024-09-05 23:49:03 +0600

    payment total price calculate

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -3,9 +3,14 @@ import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
+import useCart from '../../../hooks/useCart'
 
 const stripePromise = loadStripe(import.meta.env.VITE_payment_pk);
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    const totalPrice = parseFloat(total.toFixed(2));
+
     return (
         <div className='w-full max-w-screen-md mx-auto my-12'>
             <Helmet>
@@ -13,7 +18,7 @@ const Payment = () => {
             </Helmet>
             <SectionTitle heading="Payment" subHeading="Please Provide"></SectionTitle>
             <Elements stripe={stripePromise}>
-                <CheckoutForm></CheckoutForm>
+                <CheckoutForm totalPrice={totalPrice}></CheckoutForm>
             </Elements>
         </div>
     );
