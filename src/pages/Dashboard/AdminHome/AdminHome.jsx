c8f70cd4dd6564baed5ef836eea64acf0b82b0cf
kawsar-uddin <kawsaruddin450@gmail.com>
2024-09-06 18:07:02 +0600

    display stats data on admin home

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,24 +1,109 @@
 import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 
 const AdminHome = () => {
-    const token = localStorage.getItem('access-token')
+    const token = localStorage.getItem('access-token');
+    const {user, loading} = useContext(AuthContext);
 
-    const {data: stats} = useQuery({
+    const { data: stats } = useQuery({
         queryKey: ['admin-stats'],
+        enabled: !loading,
         queryFn: async () => {
             const res = await fetch(`http://localhost:8000/admin-stats`, {
                 headers: {
                     authorization: `bearer ${token}`
                 }
-            })
-            const data = res.json();
-            return data;
+            });
+            return res.json();
         }
     })
+    const revenue = stats.revenue;
+    const users = stats.users;
+    const orders = stats.orders;
+    const products = stats.products;
+    console.log(revenue, users, orders);
     return (
         <div>
             <h2>Admin er basha..</h2>
+            <div className="stats shadow">
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block h-8 w-8 stroke-current">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                    </div>
+                    <div className="stat-title">Revenue</div>
+                    <div className="stat-value">${revenue}</div>
+                    <div className="stat-desc">Jan 1st - Feb 1st</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block h-8 w-8 stroke-current">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path>
+                        </svg>
+                    </div>
+                    <div className="stat-title">Customers</div>
+                    <div className="stat-value">{users}</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block h-8 w-8 stroke-current">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path>
+                        </svg>
+                    </div>
+                    <div className="stat-title">Products</div>
+                    <div className="stat-value">{products}</div>
+                    <div className="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block h-8 w-8 stroke-current">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path>
+                        </svg>
+                    </div>
+                    <div className="stat-title">Orders</div>
+                    <div className="stat-value">{orders}</div>
+                    <div className="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+            </div>
         </div>
     );
 };
