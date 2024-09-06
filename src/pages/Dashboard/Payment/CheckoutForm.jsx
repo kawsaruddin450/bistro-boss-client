63baec64e97e04be75bc862e9968ed3e82dd1d21
kawsar-uddin <kawsaruddin450@gmail.com>
2024-09-06 12:59:02 +0600

    implement confirm card payment to process payment

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,11 +1,31 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 
 const CheckoutForm = ({totalPrice}) => {
     const stripe = useStripe();
     const elements = useElements();
     const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    const token = localStorage.getItem('access-token');
+    const {user} = useContext(AuthContext);
+
+    useEffect(()=> {
+        fetch(`http://localhost:8000/create-payment-intent`, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                authorization: `bearer ${token}`
+            },
+            body: JSON.stringify({price: totalPrice})
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setClientSecret(data.clientSecret);
+        })
+    },[token, totalPrice])
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -28,6 +48,19 @@ const CheckoutForm = ({totalPrice}) => {
             setCardError('');
             console.log('payment method:', paymentMethod);
         }
+        const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(clientSecret, {
+            payment_method:{
+                card: card,
+                billing_details: {
+                    name: user?.displayName || "unknown",
+                    email: user?.email || "annonymus",
+                }
+            }
+        });
+        if(confirmError){
+            setCardError(confirmError);
+        }
+        console.log(paymentIntent);
     }
     return (
         <>
@@ -48,7 +81,7 @@ const CheckoutForm = ({totalPrice}) => {
                         },
                     }}
                 />
-                <button className="btn bg-[#D1A054] btn-sm mt-5 text-white hover:bg-[#D1A054]" type="submit" disabled={!stripe}>
+                <button className="btn bg-[#D1A054] btn-sm mt-5 text-white hover:bg-[#D1A054]" type="submit" disabled={!stripe || !clientSecret}>
                     Pay
                 </button>
             </form>
